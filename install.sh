750861970ead74183dc3daae36e72998b13747e3
Aron Cedercrantz <cedercra@spotify.com>
2016-02-19 17:19:11 +0100

    Remove linux install function

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-linux() {
-  hash shellcheck 2>/dev/null || { echo >&2 "I require shellcheck but it wasn’t installed. Aborting."; exit 1; }
-}
-
 osx() {
   brew update
   brew install shellcheck
@@ -12,6 +8,4 @@ osx() {
 
 if [ "$(uname -s)" = "Darwin" ]; then
   osx
-else
-  linux
 fi
